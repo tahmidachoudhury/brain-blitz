62a37d12efb1b68fe1d76f80c79f221def027e08
Tahmid Choudhury <tahmidachoudhury@outlook.com>
2024-07-20 01:06:43 +0100

    gartic phone ui back

--- frontend/quiz-app/src/components/LandingPage/index.tsx
+++ frontend/quiz-app/src/components/LandingPage/index.tsx
@@ -1,14 +1,16 @@
-import { Box } from "@mui/material";
-import ResponsiveAppBar from "../ResponsiveAppbar";
-import React from "react";
-import CallToAction from "./CallToAction";
+import { Box } from "@mui/material"
+import ResponsiveAppBar from "../ResponsiveAppbar"
+import React from "react"
+import CallToAction from "./CallToAction"
+import ChooseGameSection from "./GameSelect"
 
 function LandingPage() {
   return (
     <Box>
       <CallToAction />
+      <ChooseGameSection />
     </Box>
-  );
+  )
 }
 
-export default LandingPage;
+export default LandingPage
